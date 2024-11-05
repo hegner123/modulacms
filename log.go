a3d584059548fb31dab31e0eb3f01d8b4e87113d
Michael Hegner <hegner123@gmail.com>
2024-11-05 03:21:58 -0500

    cleared experiement

--- log.go
+++ log.go
@@ -2,18 +2,3 @@ package main
 
 
 
-type Logger struct{
-    Id string
-}
-
-
-func (Logger)NewLogger()Logger{
-    logger := Logger{Id:timestamp()}
-    return logger
-}
-
-func (Logger)EnableLog(verbose bool)
-
-
-var Logg Logger = Logger{}.NewLogger()
-
