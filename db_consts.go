915c528a19b5791253c09061c8119e41ea7d5ca8
Michael Hegner <hegner123@gmail.com>
2024-11-07 11:53:55 -0500

    updated consts and expected test results

--- db_consts.go
+++ db_consts.go
@@ -1,7 +1,7 @@
 package main
 
 const mediaTable string = `CREATE TABLE IF NOT EXISTS media (id INTEGER PRIMARY KEY, name TEXT NOT NULL, displayname TEXT, alt TEXT, caption TEXT, description TEXT, class TEXT, author TEXT, authorid INTEGER, datecreated TEXT, datemodified TEXT, url TEXT, mimeType TEXT, dimensions TEXT, optimizedmobile TEXT, optimizedtablet TEXT, optimizeddesktop TEXT, optimizedultrawide TEXT);`
-const userTable string = `CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY , datecreated TEXT , datemodified TEXT, username TEXT, name TEXT, email TEXT, hash TEXT, role TEXT);`
+const userTable string = `CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, datecreated TEXT, datemodified TEXT, username TEXT, name TEXT, email TEXT, hash TEXT, role TEXT);`
 const adminRoutesTable string = `CREATE TABLE IF NOT EXISTS adminroutes (id INTEGER PRIMARY KEY, slug TEXT NOT NULL, author TEXT, authorId INTEGER, title TEXT, status INTEGER NOT NULL, datecreated TEXT NOT NULL, datemodified TEXT NOT NULL, content TEXT NOT NULL, type TEXT NOT NULL, template TEXT);`
 const routesTable string = `CREATE TABLE IF NOT EXISTS routes (id INTEGER PRIMARY KEY, slug TEXT NOT NULL, author TEXT, authorId INTEGER, title TEXT, status INTEGER NOT NULL, datecreated TEXT NOT NULL, datemodified TEXT NOT NULL, content TEXT NOT NULL, type TEXT NOT NULL, template TEXT);`
 const fieldsTable string = `CREATE TABLE IF NOT EXISTS fields(id INTEGER PRIMARY KEY, routeId INTEGER NOT NULL, author TEXT, authorId TEXT, key TEXT, data TEXT, datecreated TEXT, datemodified TEXT, component TEXT, tags TEXT, parent TEXT);`
--- db_formats_test.go
+++ db_formats_test.go
@@ -20,8 +20,8 @@ func TestDbCreateTable(t *testing.T) {
 func TestReflection(t *testing.T) {
 	user := User{UserName: "example", Name: "example", Email: "example@mail.com", Hash: "laksdbgoiabjkb", Role: "admin", DateCreated: "1730634309", DateModified: "1730634309"}
 	result, fieldsLength := formatSQLColumns(user)
-	expected := "(username, name, email, hash, role, dateCreated, dateModified)"
-	expectedLength := 7
+	expected := "(id, dateCreated, dateModified, username, name, email, hash, role)"
+	expectedLength := 8
 	if result != expected && fieldsLength != int64(expectedLength) {
 		t.Errorf("Reflect the user struct into SQL column syntax. \nLen: %d Result:%s \nWant\nLen:%d SQL:%s\n", fieldsLength, result, expectedLength, expected)
 	}
